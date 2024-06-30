8989119bcc596e06f1b9e221b6d0736c4980c7db
Marcus Weiner <marcus.weiner@gmail.com>
2024-06-30 22:18:50 +0200

    Install BGP plugin

--- docker/plugins.py
+++ docker/plugins.py
@@ -1,7 +1,10 @@
-PLUGINS = ["netbox_secrets"]
+PLUGINS = ["netbox_secrets", "netbox_bgp"]
 
 PLUGINS_CONFIG = {
     'netbox_secrets': {
         'display_default': 'right_page',
+    },
+    'netbox_bgp': {
+        'top_level_menu': True,
     },
 }
