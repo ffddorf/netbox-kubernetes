5e37910f80c873d89e60c3d56a1414bd44272d99
Marcus Weiner <marcus.weiner@gmail.com>
2024-09-21 22:32:26 +0200

    Allow setting extra allowed chars

--- docker/extra.py
+++ docker/extra.py
@@ -2,9 +2,15 @@ import sys
 sys.path.append('/etc/netbox/config/modules')
 
 from ansible_collections.ansible.utils.plugins.plugin_utils.base.ipaddr_utils import ipaddr
-from slugify import slugify
+from slugify import slugify, SLUG_OK
+
+def slugify_filter(input, **kwargs):
+  if "extra_chars" in kwargs:
+    kwargs["ok"] = SLUG_OK+kwargs["extra_chars"]
+    del kwargs["extra_chars"]
+  slugify(input, **kwargs)
 
 JINJA2_FILTERS = {
     'ipaddr': ipaddr,
-    'slugify': slugify,
+    'slugify': slugify_filter,
 }
