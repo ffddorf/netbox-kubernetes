2717f8a0f24b8993455b70b0167f3ff53f0d2e22
Marcus Weiner <marcus.weiner@gmail.com>
2024-09-21 22:19:09 +0200

    Provide slugify filter

--- docker/extra.py
+++ docker/extra.py
@@ -2,7 +2,9 @@ import sys
 sys.path.append('/etc/netbox/config/modules')
 
 from ansible_collections.ansible.utils.plugins.plugin_utils.base.ipaddr_utils import ipaddr
+from slugify import slugify
 
 JINJA2_FILTERS = {
     'ipaddr': ipaddr,
+    'slugify': slugify,
 }
