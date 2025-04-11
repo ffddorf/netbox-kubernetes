00bd8d5376fc1dfc97ce764387c6fc1ffa045bd4
Marcus Weiner <marcus.weiner@gmail.com>
2025-04-11 23:48:42 +0200

    Upgrade netbox to 4.1.11 (#25)
    * Upgrade netbox to 4.1.11
    
    * Update healthcheck path

--- docker/plugins.py
+++ docker/plugins.py
@@ -1,10 +1,10 @@
 PLUGINS = ["netbox_secrets", "netbox_bgp"]
 
 PLUGINS_CONFIG = {
-    'netbox_secrets': {
-        'display_default': 'right_page',
-    },
     'netbox_bgp': {
         'top_level_menu': True,
+    },
+    'netbox_secrets': {
+        'display_default': 'right_page',
     },
 }
