931828bb4fc25e7eef1210bbccde8c22ed955872
Marcus Weiner <marcus.weiner@gmail.com>
2025-11-15 13:56:15 +0100

    Build with supported slugify filter (#33)

--- docker/extra.py
+++ docker/extra.py
@@ -1,11 +1,5 @@
-from slugify import slugify, SLUG_OK
-
-def slugify_filter(input, **kwargs):
-  if "extra_chars" in kwargs:
-    kwargs["ok"] = SLUG_OK+kwargs["extra_chars"]
-    del kwargs["extra_chars"]
-  return slugify(input, **kwargs)
+from slugify import slugify
 
 JINJA2_FILTERS = {
-    'slugify': slugify_filter,
+    'slugify': slugify,
 }
